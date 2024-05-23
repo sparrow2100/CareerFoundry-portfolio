79b9745f05d0acdcc165db9bd68fc75c1ced67b1
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2024-05-23 11:58:46 -0400

    Finish portfolio update

--- exercise 1.2/js/index.js
+++ exercise 1.2/js/index.js
@@ -1,21 +1,23 @@
 const nav = document.getElementById("nav");
 const main = document.getElementById("main");
 function stickNav(event) {
-  if (window.pageYOffset > 70) {
+  if (window.pageYOffset > 80) {
     nav.style.position = "fixed";
     nav.style.zIndex = "5";
     nav.style.backgroundColor = "white";
     nav.style.boxShadow = "1px 6px 20px rgba(0, 0, 0, 0.05)";
-    nav.style.width = "100%";
+    nav.style.margin = "0 auto";
+    nav.style.width = "93%";
     nav.style.top = "0px";
-    nav.style.left = "0px";
-    nav.style.transition = "all 300ms ease-in-out";
+
+    // nav.style.transition = "all 300ms ease-in-out";
 
     main.style.paddingTop = "100px";
   } else if (window.pageYOffset < 70) {
     nav.style.position = "static";
     nav.style.boxShadow = "none";
     main.style.paddingTop = "0px";
+    nav.style.width = "100%";
   }
 }
 window.addEventListener("scroll", stickNav);
