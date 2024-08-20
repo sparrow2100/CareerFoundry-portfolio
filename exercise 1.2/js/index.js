f94fa8aa0b996f828da1e53f34162076bdf51c4a
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2024-08-19 22:31:06 -0400

    Add detailed case studies for projects

--- exercise 1.2/js/index.js
+++ exercise 1.2/js/index.js
@@ -7,10 +7,10 @@ function stickNav(event) {
     nav.style.backgroundColor = "white";
     nav.style.boxShadow = "1px 6px 20px rgba(0, 0, 0, 0.05)";
     nav.style.margin = "0 auto";
-    nav.style.width = "93%";
-    nav.style.top = "0px";
+    nav.style.maxWidth = "93%";
+    nav.style.top = "0";
 
-    // nav.style.transition = "all 300ms ease-in-out";
+    nav.style.transition = "all 300ms ease-in-out";
 
     main.style.paddingTop = "100px";
   } else if (window.pageYOffset < 70) {
